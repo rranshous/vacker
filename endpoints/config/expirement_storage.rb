8fcfe0897c4969f7c6616141242a4300043e4f35
Robby Ranshous <rranshous@gmail.com>
2014-08-17 17:19:00 -0400

    PROD config

--- endpoints/config/expirement_storage.rb
+++ endpoints/config/expirement_storage.rb
@@ -9,3 +9,13 @@ environment :development do
   end
   ::Expirement.bind config['mongo']
 end
+
+environment :production do
+  require_relative '../../lib/expirement/mongo'
+  import('mongo')
+  class ::Expirement
+    puts "BINDING MONGO"
+    prepend ::Expirement::Mongo
+  end
+  ::Expirement.bind config['mongo']
+end
--- endpoints/config/mongo.rb
+++ endpoints/config/mongo.rb
@@ -2,6 +2,7 @@ require 'em-synchrony'
 require 'em-synchrony/em-mongo'
 
 environment :development do
+  puts "MONGO DEV"
   config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
     conn = EM::Mongo::Connection.new('localhost', 27017, 1, {:reconnect_in => 1})
     conn.db('vacker')
@@ -9,6 +10,7 @@ environment :development do
 end
 
 environment :production do
+  puts "MONGO PROD"
   config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
     conn = EM::Mongo::Connection.new('localhost', 27017, 1, {:reconnect_in => 1})
     conn.db('vacker')
