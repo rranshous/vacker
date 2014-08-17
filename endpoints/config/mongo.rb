0d168f530b05f1a53187a05fb2774cb1067ab2e3
Robby Ranshous <rranshous@gmail.com>
2014-08-17 17:05:29 -0400

    prod mongo config

--- endpoints/config/mongo.rb
+++ endpoints/config/mongo.rb
@@ -2,7 +2,13 @@ require 'em-synchrony'
 require 'em-synchrony/em-mongo'
 
 environment :development do
-  puts "CONFIG MONGO"
+  config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
+    conn = EM::Mongo::Connection.new('localhost', 27017, 1, {:reconnect_in => 1})
+    conn.db('vacker')
+  end
+end
+
+environment :production do
   config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
     conn = EM::Mongo::Connection.new('localhost', 27017, 1, {:reconnect_in => 1})
     conn.db('vacker')
